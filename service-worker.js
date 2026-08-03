0c729cc2a39643b2ffd6d42cea20a62e532d455f
Relyyc <61234846+Relyyc@users.noreply.github.com>
2026-08-03 15:17:32 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,36 @@
-const CACHE_NAME = "fila-pwa-v3";
+const CACHE_NAME = "fila-pwa-v2";
 
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
-    "./manifest.json",
     "./icon-180.png",
     "./icon-512.png",
-    "./nemico.png"
+    "./nemico.png",
+    "./Coin-Block_Run.mp3"
 ];
 
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
-            .then(() => self.skipWaiting())
+        .then(cache => cache.addAll(FILES_TO_CACHE))
     );
 });
 
 self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-        )).then(() => self.clients.claim())
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                .filter(key => key !== CACHE_NAME)
+                .map(key => caches.delete(key))
+            );
+        })
     );
 });
 
 self.addEventListener("fetch", event => {
-    if (event.request.method !== "GET") return;
-
     event.respondWith(
-        fetch(event.request)
-            .then(response => {
-                if (response.ok && new URL(event.request.url).origin === self.location.origin) {
-                    const copy = response.clone();
-                    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-                }
-                return response;
-            })
-            .catch(() => caches.match(event.request).then(response => response || caches.match("./index.html")))
+        caches.match(event.request)
+        .then(response => response || fetch(event.request))
     );
 });
