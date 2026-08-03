d3cda32fd56c6b92316a2aed448449b95ee795ee
Filannino Michele <MFilannino@asst-rhodense.it>
2026-08-03 15:11:18 +0200

    fix: migliora fotocamera e aggiornamenti PWA

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,42 @@
-const CACHE_NAME = "fila-pwa-v1";
+const CACHE_NAME = "fila-pwa-v3";
 
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
+    "./manifest.json",
     "./icon-180.png",
     "./icon-512.png",
-    "./nemico.png",
-    "./Coin-Block_Run.mp3"
+    "./nemico.png"
 ];
 
-
 self.addEventListener("install", event => {
-
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
     );
-
 });
-
 
 self.addEventListener("activate", event => {
-
     event.waitUntil(
-        caches.keys().then(keys => {
-
-            return Promise.all(
-                keys
-                .filter(key => key !== CACHE_NAME)
-                .map(key => caches.delete(key))
-            );
-
-        })
+        caches.keys().then(keys => Promise.all(
+            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        )).then(() => self.clients.claim())
     );
-
 });
 
-
 self.addEventListener("fetch", event => {
+    if (event.request.method !== "GET") return;
 
     event.respondWith(
-
-        caches.match(event.request)
-        .then(response => {
-
-            return response || fetch(event.request);
-
-        })
-
+        fetch(event.request)
+            .then(response => {
+                if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+                    const copy = response.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+                }
+                return response;
+            })
+            .catch(() => caches.match(event.request).then(response => response || caches.match("./index.html")))
     );
-
 });
