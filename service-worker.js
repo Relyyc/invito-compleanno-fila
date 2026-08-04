9e867aff863909c81993fbcfc92580aeeeff68e7
Relyyc <61234846+Relyyc@users.noreply.github.com>
2026-08-04 10:08:30 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fila-pwa-v2";
+const CACHE_NAME = "fila-pwa-v1";
 
 const FILES_TO_CACHE = [
     "./",
@@ -9,28 +9,45 @@ const FILES_TO_CACHE = [
     "./Coin-Block_Run.mp3"
 ];
 
+
 self.addEventListener("install", event => {
+
     event.waitUntil(
         caches.open(CACHE_NAME)
         .then(cache => cache.addAll(FILES_TO_CACHE))
     );
+
 });
 
+
 self.addEventListener("activate", event => {
+
     event.waitUntil(
         caches.keys().then(keys => {
+
             return Promise.all(
                 keys
                 .filter(key => key !== CACHE_NAME)
                 .map(key => caches.delete(key))
             );
+
         })
     );
+
 });
 
+
 self.addEventListener("fetch", event => {
+
     event.respondWith(
+
         caches.match(event.request)
-        .then(response => response || fetch(event.request))
+        .then(response => {
+
+            return response || fetch(event.request);
+
+        })
+
     );
+
 });
